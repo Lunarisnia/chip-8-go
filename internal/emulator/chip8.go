1129b4d93bf9f96ae0bc3b0dfddc7da268f09711
louna <r.arswendo.r@gmail.com>
2024-08-02 18:02:05 +0700

    feat: added fontset and debug func

--- internal/emulator/chip8.go
+++ internal/emulator/chip8.go
@@ -1,5 +1,10 @@
 package emulator
 
+import (
+	"fmt"
+	"github.com/Lunarisnia/chip-8-go/internal/emulator/fontset"
+)
+
 const windowWidth int32 = 64
 const windowHeight int32 = 32
 
@@ -19,6 +24,24 @@ type Chip8 struct {
 }
 
 func NewChip8() *Chip8 {
-	chip8 := Chip8{}
+	chip8 := Chip8{
+		ProgramCounter: 0x200,
+		I:              0,
+		StackPointer:   0,
+		OpCode:         0,
+	}
+	for i, b := range fontset.FontSet {
+		chip8.Memory[i] = b
+	}
 	return &chip8
+}
+
+func (c *Chip8) PrintFontSet() {
+	for i := 0; i < len(fontset.FontSet); i += 5 {
+		font := c.Memory[i : i+5]
+		for _, d := range font {
+			fmt.Printf("%08b\n", d)
+		}
+		fmt.Println("")
+	}
 }
